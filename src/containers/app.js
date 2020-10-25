431eb505ea72a2f2374ae0d75cfa0bf2b6dd28b8
ben <benoit.rejou@coding-academy.fr>
2020-10-25 22:56:14 +0100

    adding VideoDetail

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react'
 import SearchBar from '../components/search-bar'
+import VideoDetail from '../components/video-details'
 import VideoList from './video-list'
 import axios from 'axios'
+
 
 const API_END_POINT = "https://api.themoviedb.org/3/"
 const POPULAR_MOVIES_URL = "discover/movie?language=fr&sort_by=popularity.desc&include_adult=false&append_to_response=images"
@@ -17,10 +19,10 @@ class App extends Component{
         axios.get(`${API_END_POINT}${POPULAR_MOVIES_URL}&${API_KEY}`).then(function(response){
             this.setState({movieList:response.data.results.slice(1,6)});
             this.setState({currentMovie:response.data.results[0]});
-            console.log('-----------------------------');
-            console.log(this.state.movieList);
-            console.log(this.state.currentMovie);
-            console.log('-----------------------------');
+            // console.log('-----------------------------');
+            // console.log(this.state.movieList);
+            // console.log(this.state.currentMovie);
+            // console.log('-----------------------------');
         }.bind(this));
     }
     render(){
@@ -28,6 +30,7 @@ class App extends Component{
                 <div>
                     <SearchBar/>
                     <VideoList />
+                    <VideoDetail title={this.state.currentMovie.title} description={this.state.currentMovie.overview}/>
                 </div>
         )
    }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './containers/app'
 
-const API_KEY='513d2a5866b142f5b79b402d27054676'
+
 
 
 
