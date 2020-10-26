8d4d419ebc3108b8e35e0659180df50133dd877c
ben <benoit.rejou@coding-academy.fr>
2020-10-26 23:37:30 +0100

    Adding recommended video

--- src/containers/app.js
+++ src/containers/app.js
@@ -55,7 +55,15 @@ class App extends Component{
     onClickListItem(movie){
         this.setState({currentMovie:movie}, function(){
             this.applyVideoToCurrentMovie();
+            this.setRecommendation();
         })
+    }
+
+    setRecommendation(){
+        
+        axios.get(`${API_END_POINT}movie/${this.state.currentMovie.id}/recommendations?${API_KEY}&language=fr`).then(function(response){
+            this.setState({movieList:response.data.results.slice(0,5)});
+        }.bind(this));
     }
 
     onClickSearch(searchText){
@@ -65,14 +73,14 @@ class App extends Component{
         if(searchText){
             axios.get(`${API_END_POINT}${SEARCH_URL}&${API_KEY}&query=${searchText}`).then(function(response){
                 if(response.data && response.data.results[0]){
-                    this.setState({movieList:response.data.results.slice(1,6), currentMovie:response.data.results[0]}, function(){
-                        this.applyVideoToCurrentMovie();
-                    });  
+                    if(response.data.results[0].id != this.state.currentMovie.id){
+
+                        this.setState({currentMovie:response.data.results[0]},()=>{
+                            this.applyVideoToCurrentMovie();
+                            this.setRecommendation();
+                        });  
+                    }
                 }
-                // console.log('-----------------------------');
-                // console.log(this.state.movieList);
-                // console.log(this.state.currentMovie);
-                // console.log('-----------------------------');
             }.bind(this));
         }
         
